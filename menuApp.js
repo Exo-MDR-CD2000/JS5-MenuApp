7c4c0848a3f293a768f8c4f482f4b43c7753915a
JAG24_Exo <joseguillen587@yahoo.com>
2024-08-25 14:45:30 -0700

    bookItem class & Library start

--- menuApp.js
+++ menuApp.js
@@ -5,3 +5,25 @@
         Your menu should have the options to create, view, and delete elements.
 
 */
+
+/** 
+ * Let's create a library app that allows users to add, view, and delete books.
+ * we can add a new book by providing the title, author, year of publication, and ISBN.
+ * we can view all books in the library.
+ * we can delete a book by the index.
+*/
+
+class BookItem { // define a class for a book item with title, author, year, and isbn
+    constructor(title, author, year, isbn) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isbn = isbn;
+    }
+}
+
+class Library { // defines a class for a library with the functionality to add, view, and delete books
+    constructor() {
+        this.books = []; // initialize an empty array to store books
+    }
+}
